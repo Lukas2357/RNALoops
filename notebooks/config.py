fe334211bcb94c2261e04859886b531674e4a5e1
LukasHunold <lukashunold@web.de>
2022-10-18 12:52:11 +0200

    Organized results and notebooks in separate subfolders

---
+++ notebooks/config.py
@@ -0,0 +1,23 @@
+from os import sys, path
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
+import pymysql
+import warnings
+from matplotlib.colors import LogNorm, Normalize
+from pandas.api.types import is_numeric_dtype
+
+PARENT_DIR = path.dirname(path.dirname(path.abspath(__file__)))
+sys.path.append(PARENT_DIR)
+
+from rnaloops.data_explorer.explore_fcts import *
+
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', 200)
+
+warnings.filterwarnings('ignore')
+
+os.chdir('..')
