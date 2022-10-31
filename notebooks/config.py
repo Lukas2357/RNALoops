35e953ec62e4f240bfce5e368a665bdd97e966b9
LukasHunold <lukashunold@web.de>
2022-10-31 10:15:22 +0100

    cleaned up

--- notebooks/config.py
+++ notebooks/config.py
@@ -1,4 +1,5 @@
-from os import sys, path
+import sys
+from os import path
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -9,18 +10,35 @@ import warnings
 from matplotlib.colors import LogNorm, Normalize
 from pandas.api.types import is_numeric_dtype
 import pickle
+from sklearn.neighbors import LocalOutlierFactor
+import colorcet as cc
+from scipy import stats
+from adjustText import adjust_text
+from timeit import default_timer
 
 from Bio.PDB import MMCIFParser, MMCIF2Dict
 
 PARENT_DIR = path.dirname(path.dirname(path.abspath(__file__)))
 sys.path.append(PARENT_DIR)
 
-from rnaloops.data_explorer.explore_fcts import *
-from rnaloops.data_verifier.mmcif_parser import *
+from rnaloops.prepare.explore_fcts import *
+from rnaloops.prepare.data_loader import *
+from rnaloops.prepare.data_preparer import *
+from rnaloops.verify.mmcif_parser import *
+from rnaloops.verify.verify_fcts import *
+from rnaloops.config.helper import *
+from rnaloops.config.constants import *
+from rnaloops.explore.plot_fcts import *
+from rnaloops.explore.get_similar import *
+from rnaloops.explore.help_fcts import *
+from rnaloops.cluster.cluster_fcts import generic_clustering
+from rnaloops.cluster.cluster_plot import do_plot
+from rnaloops.cluster.cluster_plot_help_fcts import init_subplots_plot
+
 
 os.chdir(PARENT_DIR)
 
 pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', 200)
+pd.set_option('display.max_rows', 1000)
 
 warnings.filterwarnings('ignore')
